4e70d081d9792a55dab4b04f6fa2661fb8dfdbaa
ed <edik3d@hotmail.com>
2013-04-27 22:52:05 -0400

    Fix unit test for 13

--- src/test/scala/com/home/SolutionTester.scala
+++ src/test/scala/com/home/SolutionTester.scala
@@ -66,6 +66,6 @@ class SolutionTest extends JUnitSuite {
   def encodeDirect(){
     assertEquals(
       List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)),
-      P13.encodeDirect(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))))
+      P13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
   }
 } 
